aa7c86afb8d542d210d75c8eb472a86103d7412c
MasqueradeToday <Sora@MasqueradeToday.local>
2014-01-26 17:45:49 -0500

    Initial Commit

---
+++ TicTacToe/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TicTacToe
+//
+//  Created by MasqueradeToday on 1/26/14.
+//  Copyright (c) 2014 Lianet Cuella. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
